d5aafc0b47ec217c6b31b971841d87590c4027b0
DokiDokiPB <20497934+a1091150@users.noreply.github.com>
2026-06-10 22:13:18 +0800

    Initial Commit

---
+++ ScanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScanApp
+//
+//  Created by 楊敦富 on 2026/6/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
